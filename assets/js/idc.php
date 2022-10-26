94aaeb8e6f73e483d96db88c7c69cf280c371f51
joamzin157 <jvma@aluno.ifnmg.edu.br>
2022-10-26 17:16:11 -0300

    values of us

---
+++ assets/js/idc.php
@@ -0,0 +1,32 @@
+<?php
+
+$matriz[1] = "Thúlio";
+
+for($n = 1;$n <= 30; $n++){
+  $matriz[$n] = "Thúlio";
+  print "Posição = $n | Valor = ". $matriz[$n];
+  print "<br>";
+}
+
+
+for($n = 1;$n <= 7;$n++){
+        $vA[$n] = $n;
+      }
+        $vB[1] = "Domingo";
+        $vB[2] = "Segunda";
+        $vB[3] = "Terça";
+        $vB[4] = "Quarta";
+        $vB[5] = "Quinta";
+        $vB[6] = "Sexta";
+        $vB[7] = "Sábado";
+
+      for($n = 1;$n <= 7;$n++){
+        $vC[$n] = $vA[$n] ." - ". $vB[$n];
+        print $vC[$n]." <br>";
+      }
+      
+      echo "avestruz";
+
+      ?>
+
+
