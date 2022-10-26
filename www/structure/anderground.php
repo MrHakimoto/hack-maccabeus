5a46dea7beb4352667ebf542da4007afdd828c8d
GeovanaL <gla2@aluno.ifnmg.edu.br>
2022-10-26 18:19:59 -0300

    avls

---
+++ www/structure/anderground.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Slide 5</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
+
+  <body class="container bg-light">
+
+    <h1 class="text-success">ATIVIDADES SLIDE 5 - Arrays Lineares</h1>
+    <hr>
+    <br>
+
+    <h3 class="text-primary">Questão 5 - Faça um programa que crie um vetor de inteiros de 10 posições leia os
+valores e imprima os valores onde o índice for impar.</h3>
+    <br>
+
+
+
+    <hr> <br>
+
+  </body>
+</html>
