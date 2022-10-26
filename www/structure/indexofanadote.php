a51c2932e9e18ab7765b79bc72c3f65676cc448b
GeovanaL <gla2@aluno.ifnmg.edu.br>
2022-10-26 18:25:58 -0300

    five's value us spreay

--- www/structure/indexofanadote.php
+++ www/structure/indexofanadote.php
@@ -36,8 +36,42 @@ $num = 0;
       }
         print "</table>";
 
-    ?>
+   
       }
 
+      $slq = "SELECT * FROM t_Table WHERE id != 2";
+      $num = 0;
+      print "<table border='2'>";
+      for($l = 1;$l <= 5;$l++){
+        print "<tr>";
+        for($c = 1;$c <= 5;$c++){
+          print "<td>";
+          $num++;
+          $matriz[$l][$c] = $num;
+          print $matriz[$l][$c]." ";
+          print "</td>";
+        }
+        print "</tr>";
+      }
 
-    ?>
+      $num = 0;
+      print "<table border='2'>";
+      for($l = 1;$l <= 3;$l++){
+        print "<tr>";
+        for($c = 1;$c <= 3;$c++){
+          print "<td>";
+          $num--;
+          $matriz[$l][$c] = $num;
+          if($l == $c){
+            print $matriz[$l][$c]." ";
+          }
+          print "</td>";
+        }
+        print "</tr>";
+      }
+        print "</table>";
+
+
+        print "</table>";
+
+        ?>
